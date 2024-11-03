79e6dbaf41a01c9ea9cfc1ba5ca0b00600df006f
Kingsley Chukure <151749239+winnerschapelogale@users.noreply.github.com>
2024-11-03 05:07:47 +0100

    Update winnes.js

--- winnes.js
+++ winnes.js
@@ -53,16 +53,7 @@ about.addEventListener("click",function(e){
          
    }
 });
-document.onreadystatechange = function(){
-   if(document.readyState !== "complete"){
-      document.querySelector("body").style.visibility ="hidden";
-      document.querySelector("#loader").style.visibility = "visible";
-   }
-   else{
-      document.querySelector("body").style.visibility ="visible";
-      document.querySelector("#loader").style.visibility = "hidden";
-   }
-}
+
 const menuItems = document.querySelectorAll(".list")
 
 
